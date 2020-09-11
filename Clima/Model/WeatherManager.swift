292d055ba0a8ef74120112c199928373fc40f9a2
Francis Yang <francis.yang@uchicago.edu>
2020-09-11 22:09:16 +0800

    Able to access data string from open weather in debug console.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,19 +9,28 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?&appid=cfd49b26c7c6f452453aeeb64c7eb2fc&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?&appid=cfd49b26c7c6f452453aeeb64c7eb2fc&units=metric"
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
     func performRequest(urlString: String){
         if let url = URL(string: urlString){
             let session = URLSession(configuration: . default)
-            let task = session.dataTask(with: <#T##URL#>, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
             task.resume()
         }
     }
     
-
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data{
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
 }
