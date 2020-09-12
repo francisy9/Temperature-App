a66bf2bfa70b6b8fe885f25d063aad47a7d88988
Francis Yang <francis.yang@uchicago.edu>
2020-09-13 03:14:02 +0800

    successfully decoded json file so that it prints out the city name from the JSON file in the debug console.

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Francis Yang on 13/09/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable{
+    let name: String
+    
+}
